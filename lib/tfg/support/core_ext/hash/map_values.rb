68c5abb4588bc9d26b8450e0f4c18b55a0727442
Thomas Drake-Brockman <thom@sfedb.com>
2013-07-24 13:34:06 +0800

    Added Hash#map_values!

--- lib/tfg/support/core_ext/hash/map_values.rb
+++ lib/tfg/support/core_ext/hash/map_values.rb
@@ -3,7 +3,7 @@ class Hash
   # keys will be retained.
   # 
   # ```ruby
-  # {foo: 1, bar: 2}.map {|value| value * 2 } #=> {foo: 2, bar: 4}
+  # {foo: 1, bar: 2}.map_values {|value| value * 2 } #=> {foo: 2, bar: 4}
   # ```
   #
   # The original hash is not mutated.
@@ -11,6 +11,21 @@ class Hash
     Hash.new.tap do |result|
       self.each do |key, value|
         result[key] = yield(value)
+      end
+    end
+  end
+
+  # The same as {Hash#map_values}, except mutates and returns the original hash.
+  # 
+  # ```ruby
+  # hash = {foo: 1, bar: 2}
+  # hash.map_values {|value| value * 2 }
+  # hash #=> {foo: 2, bar: 4}
+  # ```
+  def map_values!
+    self.tap do |hash|
+      hash.each do |key, value|
+        hash[key] = yield(value)
       end
     end
   end
